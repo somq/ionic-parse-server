69adbb07ac40853e31bdd7a755db2770063aca26
somq <mail.clement@gmail.com>
2018-03-02 19:33:42 +0100

    testing sdk

--- src/providers/parse/parse.ts
+++ src/providers/parse/parse.ts
@@ -10,10 +10,7 @@ import { Parse } from 'parse';
 
 // Constants
 import { ENV } from '../../app/app.constant';
-<<<<<<< HEAD
 import { Subject } from 'rxjs/Subject';
-=======
->>>>>>> 71b7dbcaa725db26294e11e4a691f9f493e7f1ab
 
 @Injectable()
 export class ParseProvider {
@@ -23,7 +20,6 @@ export class ParseProvider {
   constructor() {
     this.parseInitialize();
     console.log('Initiated Parse');
-<<<<<<< HEAD
 
 
   }
@@ -47,10 +43,6 @@ export class ParseProvider {
     })
 
   }
-=======
-  }
-
->>>>>>> 71b7dbcaa725db26294e11e4a691f9f493e7f1ab
   public getGameScores(offset: number = 0, limit: number = 3): Promise<any> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
